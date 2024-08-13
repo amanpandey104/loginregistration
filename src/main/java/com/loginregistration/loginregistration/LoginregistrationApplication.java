41aaf236fc2935da36382ac3cc4ef0bc52f5d221
amanpandey104 <36291873+amanpandey104@users.noreply.github.com>
2024-08-14 01:29:52 +0530

    updated Java verson to java22

--- src/main/java/com/loginregistration/loginregistration/LoginregistrationApplication.java
+++ src/main/java/com/loginregistration/loginregistration/LoginregistrationApplication.java
@@ -3,6 +3,7 @@ package com.loginregistration.loginregistration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(scanBasePackages={"com.loginregistration.loginregistration.Service.EmployeeService"})
 public class LoginregistrationApplication {
 
